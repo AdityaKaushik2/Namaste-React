3e207605da6a1ced337ebb66613b7641ea61f710
AdityaKaushik2 <aditya.kaushik2505@gmail.com>
2023-07-05 10:28:42 +0530

    3rd commit

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
+
+//React.createElement =>Object => HTML(DOM)
 const Heading = React.createElement(
   "h1",
   {
@@ -7,6 +9,44 @@ const Heading = React.createElement(
   },
   "Kaise Ho Bhaiya"
 );
+
+// Above Code in JSX
+//JSX => React.createElement =>Object => HTML(DOM)
+//Babel Understands JSX
+//Babel comes along with parcel as dependency
+
+
+//This Is a React Element
+const Heading2 = (
+  <h1 id="Hello" key="h2">
+    Namaste React
+  </h1>
+);
+
+//Functional Componenets
+//Name Of Component should start with capital letter -- Good Practice
+//
+const HeaderComponent = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+        <h2>My Name is Aditya</h2>
+      </div>
+    </>
+  );
+};
+
+//Another Way to write Arrow Function
+const HeaderComponent2 = () => {
+  <>
+    <div>
+      <h1>Hello</h1>
+      <h2>My Name is Aditya</h2>
+    </div>
+  </>;
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //Passing A React Element inside The Root
-root.render(Heading);
+root.render(<HeaderComponent/ >);
