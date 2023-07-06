e1beddb0a60aeee15a066f20e38d9ea00043f51b
AdityaKaushik2 <aditya.kaushik2505@gmail.com>
2023-07-06 19:57:37 +0530

    Assignment 4 practice

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 //React.createElement =>Object => HTML(DOM)
-const Heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "Kaise Ho Bhaiya"
-);
+// const Heading = React.createElement(
+//   "h1",
+//   {
+//     id: "title",
+//   },
+//   "Kaise Ho Bhaiya"
+// );
 
 // Above Code in JSX
 //JSX => React.createElement =>Object => HTML(DOM)
@@ -17,36 +17,81 @@ const Heading = React.createElement(
 
 
 //This Is a React Element
-const Heading2 = (
-  <h1 id="Hello" key="h2">
-    Namaste React
-  </h1>
-);
+// const Heading2 = (
+//   <h1 id="Hello" key="h2">
+//     Namaste React
+//   </h1>
+// );
 
 //Functional Componenets
 //Name Of Component should start with capital letter -- Good Practice
 //
+// const HeaderComponent = () => {
+//   return (
+//     <>
+//       <div>
+//         <h1>Hello</h1>
+//         <h2>My Name is Aditya</h2>
+//       </div>
+//     </>
+//   );
+// };
+
+//Another Way to write Arrow Function
+// const HeaderComponent2 = () => {
+//   <>
+//     <div>
+//       <h1>Hello</h1>
+//       <h2>My Name is Aditya</h2>
+//     </div>
+//   </>;
+// };
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+//Passing A React Element inside The Root
+// root.render(<HeaderComponent/ >);
+
+
+// Assignment 4 Solutions
+// Q1 -Create a Nested header Element using React.createElement(h1,h2,h3 inside a div with class “title”)
+
+const Header = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  React.createElement("h1",{class : "as"},"1st Header"),
+  React.createElement("h2",{class : "titasle"},"2nd Header"),
+  React.createElement("h3",{class : "titlsde"},"3rd Header")
+);
+
+const root = ReactDOM.createRoot(document.getElementById("title"));
+// root.render(Header);
+
+//Same Element Using JSX
 const HeaderComponent = () => {
   return (
     <>
       <div>
-        <h1>Hello</h1>
+        <h1 style={{color: "red"}}>Hello</h1>
         <h2>My Name is Aditya</h2>
+        <h3>My Name is Aditya Kaushik</h3>
+      </div>
+    </>
+  );
+};
+const HeaderComponent2 = () => {
+  return (
+    <>
+      <div>
+        <HeaderComponent/>
+        <h1 style={{color: "blue"}}>Hello</h1>
+        <h2>My Name is Aditya</h2>
+        <h3>My Name is Aditya Kaushik</h3>
       </div>
     </>
   );
 };
 
-//Another Way to write Arrow Function
-const HeaderComponent2 = () => {
-  <>
-    <div>
-      <h1>Hello</h1>
-      <h2>My Name is Aditya</h2>
-    </div>
-  </>;
-};
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-//Passing A React Element inside The Root
-root.render(<HeaderComponent/ >);
+root.render(<HeaderComponent2/>);
